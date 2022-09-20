38c9fd5d9d6013b9f7e737bbb704da1668283bad
Ax-47 <arminglwnza@gmail.com>
2022-09-20 13:00:36 +0700

    dsaf

--- api/path/setting_edit/github.go
+++ api/path/setting_edit/github.go
@@ -21,14 +21,32 @@ func SETGithub(c *gin.Context, s db.Db_mongo) {
 		return
 	}
 	u, err := url.ParseRequestURI(file)
-	if u.Hostname() != "github.com" {
+	if u.Hostname() == "github.com" {
+		s.Db_FixOneStuck(bson.M{"username": bson.M{"$eq": key.Claims.(jwt.MapClaims)["aud"].(string)}, "tag": bson.M{"$eq": key.Claims.(jwt.MapClaims)["jti"].(string)}, "email": bson.M{"$eq": key.Claims.(jwt.MapClaims)["iss"].(string)}},
+			bson.M{"$set": bson.M{"subdata.Github": file}})
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Your file has been successfully uploaded.",
+		})
+		return
+	} else if u.Hostname() == "facebook.com" {
+		s.Db_FixOneStuck(bson.M{"username": bson.M{"$eq": key.Claims.(jwt.MapClaims)["aud"].(string)}, "tag": bson.M{"$eq": key.Claims.(jwt.MapClaims)["jti"].(string)}, "email": bson.M{"$eq": key.Claims.(jwt.MapClaims)["iss"].(string)}},
+			bson.M{"$set": bson.M{"subdata.facebook": file}})
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Your file has been successfully uploaded.",
+		})
+		return
+	} else if u.Hostname() == "youtube.com" {
+		s.Db_FixOneStuck(bson.M{"username": bson.M{"$eq": key.Claims.(jwt.MapClaims)["aud"].(string)}, "tag": bson.M{"$eq": key.Claims.(jwt.MapClaims)["jti"].(string)}, "email": bson.M{"$eq": key.Claims.(jwt.MapClaims)["iss"].(string)}},
+			bson.M{"$set": bson.M{"subdata.youtube": file}})
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Your file has been successfully uploaded.",
+		})
+		return
+	} else {
 		c.AbortWithStatus(404)
 		return
 	}
-	s.Db_FixOneStuck(bson.M{"username": bson.M{"$eq": key.Claims.(jwt.MapClaims)["aud"].(string)}, "tag": bson.M{"$eq": key.Claims.(jwt.MapClaims)["jti"].(string)}, "email": bson.M{"$eq": key.Claims.(jwt.MapClaims)["iss"].(string)}},
-		bson.M{"$set": bson.M{"subdata.Github": file}})
+
 	// File saved successfully. Return proper result
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Your file has been successfully uploaded.",
-	})
+
 }
