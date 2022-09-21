5601efc49c6a75ea79c5fb9af5c2facce1561809
Ax-47 <arminglwnza@gmail.com>
2022-09-21 22:25:13 +0700

    asdf

---
+++ api/path/Profile_Dash/main.go
@@ -0,0 +1,11 @@
+package Dash
+
+// import (
+// 	db "api/db"
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func Dash(c *gin.Context,s db.Db_mongo){
+// 	ID := c.Param("ID")
+// 	ds,er:=s.Db_FindALL("",ID)
+// }
